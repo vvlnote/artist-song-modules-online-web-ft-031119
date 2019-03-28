b756aff2d7aa0cf03510800fe81462ad7ee3b52f
Joy Lin <vvlnote@hotmail.com>
2019-03-28 16:50:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,13 +23,6 @@ class Artist
     @@artists
   end
 
-  #def self.reset_all
-  #  self.all.clear
-  #end
-
-  #def self.count
-  #  self.all.count
-  #end
 
   def add_song(song)
     @songs << song
