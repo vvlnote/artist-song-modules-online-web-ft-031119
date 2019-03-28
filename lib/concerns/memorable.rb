ceddf4ee76ae4a79fff4325875f687728edec787
Joy Lin <vvlnote@hotmail.com>
2019-03-28 17:45:04 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,12 @@
 module Memorable
+  module ClassMethods
   
-  def reset_all
-    all.clear
-  end
-  
-  def count
-    all.length
+    def reset_all
+      all.clear
+    end
+    
+    def count
+      all.length
+    end
   end
 end
