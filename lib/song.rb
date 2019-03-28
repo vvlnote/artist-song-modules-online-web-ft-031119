5aa287713ba37d5f75f7f4ed711d6cb259f99f1a
Joy Lin <vvlnote@hotmail.com>
2019-03-28 17:01:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 class Song
   
-  extend Memorable
+  extend Memorable, Findable
   
   attr_accessor :name
   attr_reader :artist
@@ -14,9 +14,9 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  #def self.find_by_name(name)
+  #  @@songs.detect{|a| a.name == name}
+  #end
 
   def self.all
     @@songs
